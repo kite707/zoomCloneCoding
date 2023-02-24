f0a81c82bfbb23543eb0e8ad9c04c9c5d8e654eb
kite707 <kite707707@gmail.com>
2023-02-24 13:23:48 +0900

    1.3 WebSocket Events

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,2 +1,12 @@
 //프론트엔드 코드
 alert("hi");
+const websocket = new WebSocket(`ws://${window.location.host}`)
+websocket.addEventListener("open",()=>{
+    console.log("Connected to Server");
+})
+websocket.addEventListener("message",(message)=>{
+    console.log("i got message, it says",message.data);
+})
+websocket.addEventListener("close",()=>{
+    console.log("disconnected from server");
+})
--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 //서버단 코드
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
@@ -7,9 +9,18 @@ const app = express();
 app.set('view engine',"pug"); //확장자가 pug인 파일을 고르겠다.
 app.set('views',__dirname+"/views");   //views폴더의 위치는 __dirname/views이다.
 app.get("/",(req,res)=>res.render("home"))
-app.get("/*",(req,res)=>res.redirect("/")) // 홈말고 다른 링크로 접근하면 리다이렉트 시킨다.
+//app.get("/*",(req,res)=>res.redirect("/")) // 홈말고 다른 링크로 접근하면 리다이렉트 시킨다.
 
 app.use("/public",express.static(__dirname+"/public"));
 
 const handleListen = () => console.log('server is running');
-app.listen(3000);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+
+wss.on('connection',(socket)=>{
+    console.log("socket connected on server.js");
+    console.log("Connected to Browser");
+    socket.send("hello! i'm backend socket");
+})
+server.listen(3000,handleListen);
