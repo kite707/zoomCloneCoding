9a3374fdf66cc65d2ad2010273e4c9e4240cc8e6
kite707 <kite707707@gmail.com>
2023-03-05 18:03:27 +0900

    feat: room name 입력 받아 socket.io를 이용한 백엔드 전송 및 서버 작업 완료 인식 기능

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,14 @@
 const socket = io();
+
+const welcome = document.getElementById('welcome');
+const form = welcome.querySelector('form');
+
+form.addEventListener('submit', handleRoomNameSubmit);
+function handleRoomNameSubmit(event) {
+  event.preventDefault();
+  const roomName = form.querySelector('input');
+  socket.emit('room', { payload: roomName.value }, () => {
+    console.log('server is done, function name is end');
+  });
+  roomName.value = '';
+}
