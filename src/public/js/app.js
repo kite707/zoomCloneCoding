a087405a4511a062e084d14ba4b597df8f3e8198
kite707 <kite707707@gmail.com>
2023-03-05 14:47:34 +0900

    fix: prettier 적용에 따른 코드 포멧 변경

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,35 +1,44 @@
 //프론트엔드 코드
-const messageList = document.querySelector("ul");
-const nickForm = document.querySelector("#nick");
-const messageForm = document.querySelector("#message");
+const messageList = document.querySelector('ul');
+const nickForm = document.querySelector('#nick');
+const messageForm = document.querySelector('#message');
+const websocket = new WebSocket(`ws://${window.location.host}`);
 
-
-const websocket = new WebSocket(`ws://${window.location.host}`)
-websocket.addEventListener("open",()=>{
-    console.log("Connected to Server");
-})
-websocket.addEventListener("message",(message)=>{
-    //console.log("i got message, it says",message.data);
-    const li = document.createElement("li");
-    li.innerHTML=message.data;
-    messageList.append(li);
-})
-websocket.addEventListener("close",()=>{
-    console.log("disconnected from server");
-})
-
-setTimeout(()=>{
-    websocket.send("hello! i'm browser socket!!");
-},10000)//10초뒤 위와 같은 메시지 보냄.
-
-function handleMessageSubmit(event){
-    event.preventDefault();
-    const input = messageForm.querySelector("input");
-    console.log(input.value);
-    websocket.send(input.value);
-    input.value="";
-
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
 }
 
-messageForm.addEventListener("submit",handleMessageSubmit)
-nickForm.addEventListener("submit",handleNickSubmit)
+websocket.addEventListener('open', () => {
+  console.log('Connected to Server');
+});
+websocket.addEventListener('message', message => {
+  //console.log("i got message, it says",message.data);
+  const li = document.createElement('li');
+  li.innerHTML = message.data;
+  messageList.append(li);
+});
+websocket.addEventListener('close', () => {
+  console.log('disconnected from server');
+});
+
+// setTimeout(()=>{
+//     websocket.send("hello! i'm browser socket!!");
+// },10000)//10초뒤 위와 같은 메시지 보냄.
+
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector('input');
+  websocket.send(makeMessage('new_message', input.value));
+  input.value = '';
+}
+
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector('input');
+  websocket.send(makeMessage('nickName', input.value));
+  input.value = '';
+}
+
+messageForm.addEventListener('submit', handleMessageSubmit);
+nickForm.addEventListener('submit', handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,35 +1,35 @@
 //서버단 코드
-import express from "express";
-import http from "http";
-import WebSocket from "ws";
+import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
 
 const app = express();
 
 //pug 설정
-app.set('view engine',"pug"); //확장자가 pug인 파일을 고르겠다.
-app.set('views',__dirname+"/views");   //views폴더의 위치는 __dirname/views이다.
-app.get("/",(req,res)=>res.render("home"))
+app.set('view engine', 'pug'); //확장자가 pug인 파일을 고르겠다.
+app.set('views', __dirname + '/views'); //views폴더의 위치는 __dirname/views이다.
+app.get('/', (req, res) => res.render('home'));
 //app.get("/*",(req,res)=>res.redirect("/")) // 홈말고 다른 링크로 접근하면 리다이렉트 시킨다.
 
-app.use("/public",express.static(__dirname+"/public"));
+app.use('/public', express.static(__dirname + '/public'));
 
-const handleListen = () => console.log('server is running');
+const handleListen = () => console.log('server is running on port 3000');
 
 const server = http.createServer(app);
-const wss = new WebSocket.Server({server});
+const wss = new WebSocket.Server({ server });
 
 const sockets = [];
 
-wss.on('connection',(socket)=>{
-    console.log("Connected to Browser");
-    sockets.push(socket);
-    socket.on("close",()=>console.log("disconnected from browser")); //브라우저 창을 닫으면 실행됨. 이벤트리스너와 같음.
-    socket.on("message",(message)=>{
-        console.log(message.toString('utf-8'));
-        //socket.send(message.toString('utf-8')) //나에게만 메시지를 다시 보내주는 코드
-        sockets.forEach((asocket)=>{
-            asocket.send(message.toString('utf-8')); //연결된 모든 connection에게 메시지를 보내줌
-        })
-    })
-})
-server.listen(3000,handleListen);
+wss.on('connection', socket => {
+  console.log('Connected to Browser');
+  sockets.push(socket);
+  socket.on('close', () => console.log('disconnected from browser')); //브라우저 창을 닫으면 실행됨. 이벤트리스너와 같음.
+  socket.on('message', message => {
+    console.log(message.toString('utf-8'));
+    //socket.send(message.toString('utf-8')) //나에게만 메시지를 다시 보내주는 코드
+    sockets.forEach(asocket => {
+      asocket.send(message.toString('utf-8')); //연결된 모든 connection에게 메시지를 보내줌
+    });
+  });
+});
+server.listen(3000, handleListen);
