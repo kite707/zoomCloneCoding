a71dc6d3545a30405bffaffcc7cde9dc6f8439b5
kite707 <12201680@inha.edu>
2024-04-10 18:22:28 +0900

    feat: 비디오, 음소거 기능 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,46 @@
 const socket = io();
+
+const myFace = document.getElementById('myFace');
+const muteBtn = document.getElementById('mute');
+const cameraBtn = document.getElementById('camera');
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+
+async function getMedia() {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+    console.log(myStream);
+    myFace.srcObject = myStream;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function handleMuteClick() {
+  myStream.getAudioTracks().forEach(track => (track.enabled = !track.enabled));
+  if (!muted) {
+    muteBtn.innerText = 'Unmute';
+    muted = true;
+  } else {
+    muteBtn.innerText = 'Mute';
+    muted = false;
+  }
+}
+
+function handleCameraClick() {
+  myStream.getVideoTracks().forEach(track => (track.enabled = !track.enabled));
+  if (!cameraOff) {
+    muteBtn.innerText = 'Turn Camera On';
+    cameraOff = true;
+  } else {
+    muteBtn.innerText = 'Turn Camera Off';
+    cameraOff = false;
+  }
+}
+
+muteBtn.addEventListener('click', handleMuteClick);
+cameraBtn.addEventListener('click', handleCameraClick);
+
+getMedia();
--- src/server.js
+++ src/server.js
@@ -1,18 +1,17 @@
-//서버단 코드
-import express from 'express';
 import http from 'http';
 import SocketIO from 'socket.io';
+import express from 'express';
+
 const app = express();
 
-//pug 설정
-app.set('view engine', 'pug'); //확장자가 pug인 파일을 고르겠다.
-app.set('views', __dirname + '/views'); //views폴더의 위치는 __dirname/views이다.
-app.get('/', (req, res) => res.render('home'));
-app.get('/*', (req, res) => res.redirect('/')); // 홈말고 다른 링크로 접근하면 리다이렉트 시킨다.
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/'));
 
 const httpServer = http.createServer(app);
-const wsServer = new SocketIO(httpServer);
+const wsServer = SocketIO(httpServer);
 
-const handleListen = () => console.log('server is running on port 3000');
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
