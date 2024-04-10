fc7414259b5e5419e83a58d469a79c8d25469b64
kite707 <12201680@inha.edu>
2024-04-10 16:16:57 +0900

    feat: public rooms 구현

--- src/server.js
+++ src/server.js
@@ -18,6 +18,20 @@ const handleListen = () => console.log('server is running on port 3000');
 const httpServer = http.createServer(app);
 const wsServer = SocketIo(httpServer);
 
+function publicRooms(){
+  const {sockets:{adapter:{sids,rooms}}}=wsServer;
+  // const sids = wsServer.sockets.adapter.sids;
+  // const rooms = wsServer.sockets.adapter.rooms;
+
+  const publicRooms = [];
+  rooms.forEach((_,key)=>{
+    if(sids.get(key)===undefined){
+      publicRooms.push(key);
+    }
+  })
+  return publicRooms;
+}
+
 wsServer.on('connection', socket => {
   socket['nickname']='Anonymous'
   socket.onAny(event => {
